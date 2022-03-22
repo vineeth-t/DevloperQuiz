5fd248380ae6bb7e65ae3b9f2b449e311104bbdf
Pavan Vineethreddy Thaviti <74752572+vineeth-t@users.noreply.github.com>
2022-03-22 19:25:22 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./styles.css";
 import { NavBar } from "./components/Header/header";
+export function ListOfQuizes() {
+  return <></>;
+}
+
 export default function App() {
   return (
     <div className="App">
       <NavBar />
+      <ListOfQuizes />
     </div>
   );
 }
