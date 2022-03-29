d32902783ff12ac37ed47be0444b7645fcda82a2
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-03-29 13:41:35 +0530

    fix: questions are added to result array on click of next button

--- src/Routes/QuizQuestions/showQuizQuestions.tsx
+++ src/Routes/QuizQuestions/showQuizQuestions.tsx
@@ -4,18 +4,23 @@ import {
   Button,
   Instructions,
 } from "../../components/Instructions/instructions";
-import { quizDetails } from "../../data";
+import { useQuizContext } from "../../Contexts/QuizContext/quizContextProvider";
+import { Question } from "../../data";
 import { updateScore } from "../../utils/updateAnswers";
 import { ShowResults } from "../ResultPage/result";
 import { QuestionCard, Options, Option, QuestionText } from "./quiz.style";
 import { QuizAnswers } from "./quiz.type";
 export const QuizQuestions = () => {
+  const {
+    quizState: { quizDetails },
+  } = useQuizContext();
   const [count, setCounter] = useState(0);
   const [optionSelected, setSelectedOption] = useState("");
   const [showInstrictions, setInstructions] = useState(true);
-  const[attempetedAnswers,setAttempetedAnswers]=useState<QuizAnswers|null>(null)
+  const [attempetedAnswers, setAttempetedAnswers] =
+    useState<QuizAnswers | null>(null);
   const { id } = useParams();
-  const currentQuiz = quizDetails.listOfQuizes.find((quiz) => quiz.id === id);
+  const currentQuiz = quizDetails?.listOfQuizes?.find((quiz) => quiz.id === id);
   let lengthofQuizQuestionsArray: any = currentQuiz?.questions.length;
   return (
     <div>
@@ -31,16 +36,9 @@ export const QuizQuestions = () => {
                   <Option
                     hoverOnOptions={true}
                     optionSelected={optionSelected === optionId ? true : false}
-                    onClick={() =>
-                      updateScore(
-                        attempetedAnswers,
-                        setAttempetedAnswers,
-                        id,
-                        setSelectedOption,
-                        currentQuiz?.questions[count],
-                        optionId
-                      )
-                    }
+                    onClick={() => {
+                      setSelectedOption(optionId)
+                    }}
                   >
                     <span> {optionValue}</span>
                   </Option>
@@ -50,13 +48,25 @@ export const QuizQuestions = () => {
           </QuestionCard>
           <Button
             next={"next"}
-            onClick={() => setCounter((count: number) => count + 1)}
+            onClick={() =>
+              updateScore(
+                attempetedAnswers,
+                setAttempetedAnswers,
+                id,
+                currentQuiz?.questions[count] as Question,
+                optionSelected,
+                setCounter
+              )
+            }
           >
             Next
           </Button>
         </>
       ) : (
-        <ShowResults attempetedAnswers={attempetedAnswers} setAttempetedAnswers={setAttempetedAnswers} />
+        <ShowResults
+          attempetedAnswers={attempetedAnswers}
+          setAttempetedAnswers={setAttempetedAnswers}
+        />
       )}
     </div>
   );
