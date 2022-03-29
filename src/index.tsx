89f248efe9bc9d25d38e71c54cbaf43dab51c885
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-03-29 13:40:42 +0530

    feat: added context

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import { render } from "react-dom";
-
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QuizContextProvider } from "./Contexts/QuizContext/quizContextProvider";
 const rootElement = document.getElementById("root");
 render(
-  <Router>
-    <App />
-  </Router>,
+  <QuizContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </QuizContextProvider>,
   rootElement
 );
