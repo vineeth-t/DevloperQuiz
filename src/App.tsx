b4890a0cdd4d0e678a188e88991c508fb1053ebd
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-03-29 13:39:14 +0530

    feat: fetched data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,20 @@ import { NavBar } from "./components/Header/header";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Routes/Home/home";
 import { QuizQuestions } from "./Routes/QuizQuestions/showQuizQuestions";
+import { useEffect } from "react";
+import axios from "axios";
+import { useQuizContext } from "./Contexts/QuizContext/quizContextProvider";
+import {  Quiz } from "./data";
 
 export default function App() {
+  const{quizDispatch}=useQuizContext()
+  useEffect(()=>{
+    (async()=>{
+        const response= await axios.get<{quizDetails:Quiz}>('https://quiz-backend.vineetht.repl.co/quizData')
+        quizDispatch({type:'SET_QUIZ',payload:response.data.quizDetails})
+
+    })()
+  },[quizDispatch])
   return (
     <div className="App">
       <NavBar />
